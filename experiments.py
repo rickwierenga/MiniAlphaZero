16197b8aa5943aa5e1b7dabd407bfe4c9cbcdff3
lukaas33 <s2817489@vuw.leidenuniv.nl>
2023-05-24 23:39:31 +0200

    Updated benchmark code

--- experiments.py
+++ experiments.py
@@ -59,35 +59,36 @@ def benchmark():
     """
     
     # Load trained MCTS model
-    model = "data/connect4-model.pt"
-    net = Network(board_size=(6, 7), policy_shape=(7,), num_layers=10)
-    net.load_state_dict(torch.load(model))
-    # Read benchmark dataset
-    tests = [file for file in os.listdir() if re.match(r"Test_*", file)]
-    scores = []
-    states = []
-    for test in tests:
-        with open(test) as f:
-            for line in f:
-                # Convert state to our game state notation
-                state, score = line.strip().split(" ")
-                game = Connect4()
-                for action in state:
-                    game = game.next_state(int(action)-1)
-                state = board2state(game)
-                states.append(state)
-                scores.append(int(score))
-    # Get estimated value of each state from MCTS
-    with torch.no_grad():
-        _, values = net(torch.stack(states))
-        values = [value.item() for value in values]
-    # Normalise benchmark scores: 0 is draw, -1 is loss and 1 is win
-    scores = [np.sign(score) for score in scores]
-    # for score, value in zip(scores, values):
-    #     print(f"{score}, {value.item()}")
-    # Compare difference
-    mse = np.square(np.subtract(values, scores)).mean()
-    print(mse)
+    models = [file.path for file in os.scandir('data') if re.match(r".*connect4-model-[0-9]*.pt", file.path)]
+    for model in models:
+        net = Network(board_size=(6, 7), policy_shape=(7,), num_layers=10)
+        net.load_state_dict(torch.load(model))
+        # Read benchmark dataset
+        tests = [file.path for file in os.scandir('data') if re.match(r".*Test_L[0-9]_R[0-9]", file.path)]
+        scores = []
+        states = []
+        for test in tests:
+            with open(test, 'r') as f:
+                for line in f:
+                    # Convert state to our game state notation
+                    state, score = line.strip().split(" ")
+                    game = Connect4()
+                    for action in state:
+                        game = game.next_state(int(action)-1)
+                    state = board2state(game)
+                    states.append(state)
+                    scores.append(int(score))
+        # Get estimated value of each state from MCTS
+        with torch.no_grad():
+            _, values = net(torch.stack(states))
+            values = [value.item() for value in values]
+        # Normalise benchmark scores: 0 is draw, -1 is loss and 1 is win
+        scores = [np.sign(score) for score in scores]
+        # for score, value in zip(scores, values):
+        #     print(f"{score}, {value.item()}")
+        # Compare difference
+        rmse = np.sqrt(np.square(np.subtract(values, scores)).mean())
+        print(model, rmse)
 
 def calc_elo(score, prev_elo_1, prev_elo_2):
     """Find new elo ratings for two players based on previous and current score
@@ -195,7 +196,8 @@ def policyVsMiniMax():
     MCTSvsMiniMax(agent=policy_agent)
 
 if __name__ == "__main__":
-    test_sig_better()
+    benchmark()
     exit()
     policyVsMiniMax()
+    exit()
     MCTSvsMiniMax()
